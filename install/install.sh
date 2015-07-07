b26d4feb87387b25b41430df28a36e09cede1ce8
digits <theo@vandrimmelen.eu>
2015-07-07 22:32:51 +0200

    Several bugs resolved

---
+++ install/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+wget https://raw.githubusercontent.com/tdrimmelen/digits/master/install/install1.sh
+wget https://raw.githubusercontent.com/tdrimmelen/digits/master/install/install2.sh
+
+chmod u+x *sh
+
+install1.sh
+install2.sh
+
