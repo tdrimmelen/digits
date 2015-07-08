e15280fe04f02410b830865e874d3411a2b35a9e
digits <theo@vandrimmelen.eu>
2015-07-08 18:43:12 +0200

    * Added Srub/Real switching in mgmt ui

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,8 +2,15 @@
 
 cd /home/digits/digits
 
+echo Starting update.
+
 # Save local edits
 git stash
 
 # Update from repository
 git pull https://github.com/tdrimmelen/digits.git
+
+echo Update executed. Restarting webserver.
+
+(sleep 1 ; nohup sudo /etc/init.d/apache2 restart > /dev/null 2>&1) &
+
