db48d9d6464ee7de2484d936035d03a5cf0489f1
digits <theo@vandrimmelen.eu>
2015-07-31 21:23:32 +0200

    * Resolved bug in installer

--- install/install3.sh
+++ install/install3.sh
@@ -9,7 +9,6 @@ if [ -z "$docker" ]; then
 	sudo su - ${app} -c "git clone https://www.github.com/tdrimmelen/${app}"
 	sudo su - ${app} -c "git config --global user.email \"pi@noone.nowhere\""
 	sudo su - ${app} -c "git config --global user.name \"Pi\""
-else
 
 	#Set some files needed wrt to hardware
 	echo "dtparam=i2c1=on" >> /boot/config.txt
