fa244b847ee32cc587e889eb64d49d2ba6792010
digits <theo@vandrimmelen.eu>
2016-03-18 20:34:29 +0100

    script corrected

--- install/update11-12.sh
+++ install/update11-12.sh
@@ -8,6 +8,7 @@ if [ ! -f /home/${app}/${app}/logger.cfg ]; then
 
 	echo "Installing logger.cfg"
 	cp /home/${app}/${app}/conf/logger_tmpl.cfg /home/${app}/${app}/logger.cfg
+	chmod u+w /home/${app}/${app}/logger.cfg
 
 fi
 
@@ -15,6 +16,7 @@ if [ ! -f /home/${app}/${app}/digits.cfg ]; then
 
 	echo "Installing digits.cfg"
 	cp /home/${app}/${app}/conf/digits_tmpl.cfg /home/${app}/${app}/digits.cfg
+	chmod u+w /home/${app}/${app}/digits.cfg
 
 fi
 
